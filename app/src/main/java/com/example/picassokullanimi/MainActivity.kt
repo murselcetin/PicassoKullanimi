249a609ab2d0ffc9f82a55042e846244a996d584
Mürsel <murselcetin51@gmail.com>
2022-07-07 23:03:25 +0300

    Picasso

--- app/src/main/java/com/example/picassokullanimi/MainActivity.kt
+++ app/src/main/java/com/example/picassokullanimi/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.picassokullanimi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.picassokullanimi.databinding.ActivityMainBinding
+import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val url = "http://kasimadalan.pe.hu/yemekler/resimler/ayran.png"
+        Picasso.get().load(url).into(binding.imageView)
     }
 }
